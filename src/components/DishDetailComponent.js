fa6e9d858ea04cf65fed9a422dc099a290c2dd47
dh711 <dhairya711@gmail.com>
2021-06-05 10:52:16 +0400

    added DishDetailComponent.

---
+++ src/components/DishDetailComponent.js
@@ -0,0 +1,80 @@
+import { Component } from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+
+class DishDetail extends Component {
+    constructor(props) {
+        super(props);
+
+        console.log(props);
+    }
+
+    renderDish(dish) {
+        if (dish != null) {
+            return (
+                <div key="{dish.id}" className="col-12 col-md-5 m-1">
+                    <Card>
+                        <CardImg
+                            width="100%"
+                            src={dish.image}
+                            alt={dish.name}
+                        />
+                        <CardBody>
+                            <CardTitle>
+                                <strong>{dish.name}</strong>
+                            </CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+            );
+        } else {
+            return <div></div>;
+        }
+    }
+
+    renderComments(comments) {
+        if (comments != null) {
+            const dishComments = comments.map((comment) => {
+                return (
+                    <li>
+                        <p>"{comment.comment}"</p>
+                        <p>
+                            -- {comment.author} on{" "}
+                            {new Intl.DateTimeFormat("en-US", {
+                                year: "numeric",
+                                month: "long",
+                                day: "2-digit",
+                            }).format(new Date(comment.date))}
+                        </p>
+                    </li>
+                );
+            });
+
+            return (
+                <div className="col-12 col-md-5 m-1">
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">{dishComments}</ul>
+                </div>
+            );
+        } else {
+            return <div></div>;
+        }
+    }
+
+    render() {
+        const dish = this.props.selectedDish;
+
+        if (dish != null) {
+            return (
+                <div className="row">
+                    {this.renderDish(dish)}
+                    {this.renderComments(dish.comments)}
+                </div>
+            );
+        }
+
+        return <div></div>;
+    }
+}
+
+export default DishDetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,12 +1,6 @@
 import { Component } from "react";
-import {
-    Card,
-    CardImg,
-    CardImgOverlay,
-    CardText,
-    CardBody,
-    CardTitle,
-} from "reactstrap";
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+import DishDetail from "./DishDetailComponent";
 
 class Menu extends Component {
     constructor(props) {
@@ -15,31 +9,12 @@ class Menu extends Component {
         this.state = {
             selectedDish: null,
         };
+
+        console.log(props);
     }
 
     onDishSelect(dish) {
         this.setState({ selectedDish: dish });
-    }
-
-    renderDish(dish) {
-        if (dish != null) {
-            return (
-                <Card>
-                    <CardImg
-                        width="100%"
-                        src={dish.image}
-                        alt={dish.name}
-                        className="flex-shrink-0 me-3 mediaImg"
-                    />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        } else {
-            return <div></div>;
-        }
     }
 
     render() {
@@ -64,9 +39,7 @@ class Menu extends Component {
         return (
             <div className="container">
                 <div className="row">{menu}</div>
-                <div className="row">
-                    {this.renderDish(this.state.selectedDish)}
-                </div>
+                <DishDetail selectedDish={this.state.selectedDish} />
             </div>
         );
     }
