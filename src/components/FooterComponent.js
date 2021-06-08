0199d1d5a03b630bca6b97a6dfa00dd61df79360
dh711 <dhairya711@gmail.com>
2021-06-08 12:20:59 +0400

    added routing and navbar.

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,24 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer(props) {
     return (
         <div className="footer">
-            <div className="container">
+            <div className="container nav-bar">
                 <div className="row justify-content-center">
                     <div className="col-4 offset-1 col-sm-2">
                         <h5>Links</h5>
                         <ul className="list-unstyled">
                             <li>
-                                <a href="#">Home</a>
+                                <Link to="/home">Home</Link>
                             </li>
                             <li>
-                                <a href="#">About</a>
+                                <Link to="/aboutus">About</Link>
                             </li>
                             <li>
-                                <a href="#">Menu</a>
+                                <Link to="/menu">Menu</Link>
                             </li>
                             <li>
-                                <a href="contactus.html">Contact</a>
+                                <Link to="/contactus">Contact</Link>
                             </li>
                         </ul>
                     </div>
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,15 +1,79 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand, Jumbotron } from "reactstrap";
+import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavbarToggler,
+    Collapse,
+    NavItem,
+    Jumbotron,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isNavOpen: false,
+        };
+
+        this.toggleNav = this.toggleNav.bind(this);
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen,
+        });
+    }
+
     render() {
         return (
             <React.Fragment>
-                <Navbar dark>
+                <Navbar dark expand="md">
+                    <NavbarToggler onClick={this.toggleNav} />
                     <div className="container">
-                        <NavbarBrand href="/">
-                            Ristorante con Fusion
+                        <NavbarBrand className="mr-auto" href="/">
+                            <img
+                                src="assets/images/logo.png"
+                                height="30"
+                                width="41"
+                                alt="Ristorante con Fusion"
+                            />
                         </NavbarBrand>
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar className="nav-bar-row">
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/home">
+                                        <span className="fa fa-home">
+                                            {" "}
+                                            Home
+                                        </span>
+                                    </NavLink>
+                                    <NavLink className="nav-link" to="/aboutus">
+                                        <span className="fa fa-info">
+                                            {" "}
+                                            About Us
+                                        </span>
+                                    </NavLink>
+                                    <NavLink className="nav-link" to="/menu">
+                                        <span className="fa fa-list">
+                                            {" "}
+                                            Menu
+                                        </span>
+                                    </NavLink>
+                                    <NavLink
+                                        className="nav-link"
+                                        to="/contactus"
+                                    >
+                                        <span className="fa fa-address-card">
+                                            {" "}
+                                            Contact Us
+                                        </span>
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
                     </div>
                 </Navbar>
                 <Jumbotron>
