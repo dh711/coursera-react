cca959a3b34794dfc2979489896fe4528860ba9a
dh711 <dhairya711@gmail.com>
2021-06-15 11:38:06 +0400

    contact form now saves information when moving to different components and resets on submit.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import Footer from "./FooterComponent";
 import { connect } from "react-redux";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { addComment, fetchDishes } from "../redux/ActionCreators";
+import { actions } from "react-redux-form";
 
 const mapStateToProps = (state) => {
     return {
@@ -24,6 +25,9 @@ const mapDispatchToProps = (dispatch) => ({
         dispatch(addComment(dishId, rating, author, comment)),
     fetchDishes: () => {
         dispatch(fetchDishes());
+    },
+    resetFeedbackForm: () => {
+        dispatch(actions.reset("feedback"));
     },
 });
 
@@ -98,7 +102,15 @@ class Main extends Component {
                         component={() => <Menu dishes={this.props.dishes} />}
                     />
                     <Route path="/menu/:dishId" component={DishWithID} />
-                    <Route exact path="/contactus" component={Contact} />
+                    <Route
+                        exact
+                        path="/contactus"
+                        component={() => (
+                            <Contact
+                                resetFeedbackForm={this.props.resetFeedbackForm}
+                            />
+                        )}
+                    />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
